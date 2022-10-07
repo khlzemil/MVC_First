bde1f9a56a5c8e517b8bdd0384b0880d043848b9
khlzemil <emilmg@code.edu.az>
2022-10-07 16:55:16 +0400

    First Task

---
+++ WebApplication_First/WebApplication_First/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWork0410.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Emil";
+            ViewBag.Surname = "Quluzade";
+            ViewBag.Age = 20;
+            ViewBag.Country = "Baku";
+            ViewBag.PhoneNumber = "+994513633533";
+            return View();
+        }
+    }
+}
